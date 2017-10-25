c52f09260200240ce229f8a147b76596044e8863
SChubuk <nameserhiy@gmail.com>
2017-10-25 10:47:56 +0300

    c1

---
+++ src/main/java/guru/springframework/blog/log4j2xml/Log4J2XmlConfTest.java
@@ -0,0 +1,11 @@
+package guru.springframework.blog.log4j2xml;
+import org.testng.annotations.Test;
+
+public class Log4J2XmlConfTest {
+
+    @Test
+    public void testPerformSomeTask() throws Exception {
+        Log4J2XmlConf log4J2XmlConf = new Log4J2XmlConf();
+        log4J2XmlConf.performSomeTask();
+    }
+}
